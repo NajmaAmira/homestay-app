683bb222918dbf83f787f2f3d565515b4fcf1a96
NajmaAmira <najmamumtaz13@gmail.com>
2025-08-29 19:34:00 +0700

    add deleet room, update room

--- src/app/components/admin/room/room-table.tsx
+++ src/app/components/admin/room/room-table.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { getRooms } from "@/lib/data";
+import Image from "next/image";
+import { formatDate, formatCurrency } from "@/lib/utils";
+import { DeleteButton, EditButton } from "./button";
 
-const RoomTable = () => {
+const RoomTable = async () => {
+    const rooms = await getRooms();
+    if(!rooms?.length) return <p>No Room Found</p>
   return (
     <div className='bg-white p-4 mt-5 shadow-sm'>
         <table className='w-full divide-y divide-gray-200'>
@@ -14,13 +19,24 @@ const RoomTable = () => {
                 </tr>
             </thead>
             <tbody className='divide-y divide-gray-200'>
-                <tr className='hover:bg-gray-100'>
-                    <td className='px-6 py-4'></td>
-                    <td className='px-6 py-4'></td>
-                    <td className='px-6 py-4'></td>
-                    <td className='px-6 py-4'></td>
-                    <td className='px-6 py-4 text-right'></td>
+                {rooms.map((room) => (
+                    <tr className='hover:bg-gray-100' key={room.id}>
+                    <td className='px-6 py-4'>
+                        <div className="h-20 w-32 relative">
+                            <Image src={room.image} fill sizes="20vw" alt="room image" className="object-cover"/>
+                        </div>
+                    </td>
+                    <td className='px-6 py-4'>{room.name}</td>
+                    <td className='px-6 py-4'>{formatCurrency(room.price)}</td>
+                    <td className='px-6 py-4'>{formatDate(room.createdAt.toString())}</td>
+                    <td className='px-6 py-4 text-right'>
+                        <div className="flex items-center justify-center gap-1">
+                            <EditButton id={room.id}/>
+                            <DeleteButton id={room.id} image={room.image}/>
+                        </div>
+                    </td>
                 </tr>
+                ))}
             </tbody>
         </table>
     </div>
