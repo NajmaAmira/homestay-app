efed0ee73e96123adfcbfceab590911e2b188e9e
NajmaAmira <najmamumtaz13@gmail.com>
2025-09-09 11:15:00 +0700

    finishing deploy

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import DashboardCards from "@/app/components/admin/dashboard-cards";
+import ReservationList from "@/app/components/admin/reservation-list";
 import { Suspense } from "react";
 
 export const metadata:Metadata ={
@@ -10,8 +11,11 @@ const DashboardPage = () => {
   return (
     <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto'>
       <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
-      <Suspense fallback={<p>Loading cards...</p>}>
-      <DashboardCards/>
+      <Suspense fallback={<p>Loading Cards...</p>}>
+        <DashboardCards/>
+      </Suspense>
+      <Suspense fallback={<p>Loading Reservation...</p>}>
+        <ReservationList/>
       </Suspense>
     </div>
   );
